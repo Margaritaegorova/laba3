ca1a6b39780034606a31d99a4273d38b5139a015
Margaritaegorova <margaritaegorova2021@mail.ru>
2024-05-19 22:37:25 +0300

    Init commit

---
+++ src/data.js
@@ -0,0 +1,58 @@
+const list = [
+  {
+    id: 1,
+    title: "Пионы",
+    price: 89,
+    img: "https://florcat.ru/upload/delight.webpconverter/upload/resize_cache/iblock/0bb/500_500_1/m1z38wk0csqdapzv6bl8aob3722eot3h.JPG.webp?165443111126050",
+    amount: 1,
+    description: "Пышные благоухающие шарики-пионы, раскрывающиеся в тепле один за одним - одни из самых эффектных цветов Крупные розовые пионы, собранные в изящный букет, помогут выразить ваши чувства и запомнятся надолго",
+    rating: 0,
+  },
+  {
+    id: 2,
+    title:  "Букет из 35 роз",
+    price: 7800,
+    img: "https://florcat.ru/upload/delight.webpconverter/upload/resize_cache/iblock/615/500_500_1/615ac131c4ef542760824bdb6135b001.jpg.webp?164702057617898",
+    amount: 1,
+    description: "Букет из 35 кустовых пионовидных роз сорта Juliet изящного розово-персикового оттенка поможет выразить ваши нежные чувства.",
+    rating: 0, 
+  },
+  {
+    id: 3,
+    title:  "15 белых хризантем",
+    price: 3100,
+    img: "https://florcat.ru/upload/delight.webpconverter/upload/resize_cache/iblock/e4a/500_500_1/e4acae817aaed297b8966013bda0aab5.jpg.webp?164701878937168",
+    amount: 1,
+    description: "Букет из 15 веточек кустовых хризантем сорта Bacardi, напоминающих пышные полевые ромашки Хризантемы - одни из самый стойких цветов, что делает их любимицами многих.",
+    rating: 0, 
+  },
+  {
+    id: 4,
+    title: "Ирисы",
+    price: 3890,
+    img: "https://florcat.ru/upload/delight.webpconverter/upload/resize_cache/iblock/2af/500_500_1/2afc2796823e84f2c780ca1f054d96d9.jpeg.webp?163663051023542",
+    amount: 1,
+    description: "Букет из 25 сиреневых ирисов с пышным раскрытием, пользующихся неизменной популярностью и любовью.",
+    rating: 0, 
+  },
+  {
+    id: 5,
+    title:  "Букет розовой гортензии",
+    price: 1500,
+    img: "https://florcat.ru/upload/delight.webpconverter/upload/resize_cache/iblock/988/500_500_1/9881648153a4ef763aed11bb6773da64.jpg.webp?162728356840164",
+    amount: 1,
+    description: "Букет гортензии нежно-розового оттенка. Упругая, пышная и свежая гортензия, украшенная стильной упаковкой, радует нас стойкостью и крупным размером, а также разнообразием оттенков и фактур, в любое время года, даже когда сезон цветения еще не начался.",
+    rating: 0, 
+  },
+  {
+    id: 6,
+    title:  "Корзина с орхидеей",
+    price: 12500,
+    img: "https://florcat.ru/upload/delight.webpconverter/upload/resize_cache/iblock/5d9/500_500_1/jri1i17dv0xd56oku5osn3y9nyn4p7yd.JPG.webp?171465013838234",
+    amount: 1,
+    description: "Нежная весенняя корзина с распускающимися веточками березы, бабочками тюльпанов, роскошными орхидеями, шариками эустомы, стойкими гвоздиками, хамелациумом и крупными сортовыми розами мгновенно окунет в атмосферу майского сада, теплого ветра и нежного согревающего солнца.",
+    rating: 0, 
+  },
+];
+
+export default list;
